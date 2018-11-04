9069cf7f1f5ca9dac43e48e39cb80de5e44da1d4
Jormund <etiennebotek@gmail.com>
2018-11-04 17:00:40 +0100

    0.1.9.20181031.1930

--- automultidraw.meta.js
+++ automultidraw.meta.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-automultidraw@Jormund
 // @name           IITC plugin: Automultidraw
 // @category       Layer
-// @version        0.1.9.20181030.1405
+// @version        0.1.9.20181031.1930
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://github.com/Jormund/automultidraw/raw/master/automultidraw.meta.js
-// @downloadURL    https://github.com/Jormund/automultidraw/raw/master/automultidraw.user.js
-// @description    [2018-10-30-1405] Autodraw for multilayered fields
+// @updateURL      https://raw.githubusercontent.com/Jormund/automultidraw/master/automultidraw.meta.js
+// @downloadURL    https://raw.githubusercontent.com/Jormund/automultidraw/master/automultidraw.user.js
+// @description    [2018-10-31-1930] Autodraw for multilayered fields
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
